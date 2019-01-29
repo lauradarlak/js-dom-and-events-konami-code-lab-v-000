9139e9032d404c24058796158de38c6488d4e35d
lauradarlak <lauradarlak@gmail.com>
2019-01-29 09:07:35 -0500

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,25 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+// Keep track of index outside of the event handler.
+
+
 function init() {
   // Write your JavaScript code inside the init() function
+  let index = 0;
+  const body = document.querySelector('body');
+  body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
 
+    if (key === code[index]) {
+      index++;
+
+      if (index === code.length) {
+        alert("Hurray!");
+
+        index = 0;
+      }
+    } else {
+      index = 0;
+    }
+  });
 }
